f53129a5e9d474b2a3cdcbc0adf08e58da1c3352
isakashiori <isakashiori@isakashiorinonotopasokon.local>
2023-05-11 14:22:50 +0900

    add: ajaxでいいね機能の実装練習

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+class StaticPagesController < ApplicationController
+  def top; end
+
+  def botton
+  end
+
+  def good
+  end
+
+  # ここでパラメーターの値をfalseに変えてあげないといけない
+  
+
+  def bad
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :messages
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "static_pages#top"
+  get '/botton', to: "static_pages#botton"
+  get  '/good', to: "static_pages#good"
+  get  '/bad', to: "static_pages#bad"
+end
